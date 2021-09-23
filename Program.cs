b6ab8dc539020389ffcc63434784118623e03813
Sucheta kushwaha <67106402+suchetakushwaha@users.noreply.github.com>
2021-09-23 05:42:10 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace lengthstring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string");
+            string s = Console.ReadLine();
+            int length = 0;
+            foreach(char c in s)
+            {
+                length++;
+            }
+            Console.WriteLine("Length of string is :{0}", length);
+        }
+    }
+}
